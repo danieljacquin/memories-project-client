6cd7dd60c9c46ae8feee01e1bcfd86bb84af8392
daniel <danielcamilo853@gmail.com>
2021-09-22 18:20:51 -0500

    NavBar is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import  memories  from './assets/images/memories.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+
+import useStyle from './style';
+
 
 const App = () => {
+
+    const classes = useStyle();
+
     return (
-        <div>
-            Hello, from App
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
