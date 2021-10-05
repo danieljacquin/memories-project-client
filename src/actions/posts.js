66d565d7c67f3c45695f37e01efa80500335cff0
daniel <danielcamilo853@gmail.com>
2021-10-05 15:20:59 -0500

    i created fetchAll, createPost actions

---
+++ src/actions/posts.js
@@ -0,0 +1,23 @@
+import * as api from '../api/index';
+
+
+export const getPosts = () => async(dispatch) => {
+    try{
+        const { data } = await api.fetchPosts();
+        dispatch({type: 'FETCH_ALL', payload: data})
+    }catch(error){
+        console.log({message: error.message})
+    }
+}
+
+export const createPost = (newPost) => async(dispatch) => {
+    try{
+        const {data} = await api.createPost(newPost);
+        dispatch({type: "CREATE", payload: data});
+    }catch(error){
+        console.log({message: error.message})
+    }
+} 
+
+
+
